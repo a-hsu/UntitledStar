a00c4c7ba38b8838993c88c2b32c561af2ab69de
Sid Naik <sid.naik27@gmail.com>
2019-10-13 11:52:54 -0400

    Homing missile stuff

--- Assets/GameAssets/Scripts/ProjectileMove.cs
+++ Assets/GameAssets/Scripts/ProjectileMove.cs
@@ -4,63 +4,74 @@ using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
-    public float speed = 20f;
+    [SerializeField] public Transform target;
+    [Range(0f,2f)]public float speed = 1f;
     public float fireRate = 4f;
     public float lifeTime = 2f;
-    Transform parent;
-    Transform newPos;
-    LaserSpawn go;
-    Transform oldPos;
+    
+    public LaserSpawn go;
+
     Vector3 dir;
-    Rigidbody rb;
-    public float force = 100f;
-    // Start is called before the first frame update
-    private void Awake()
-    {
-        
-    }
+    public Rigidbody rb;
+
     void Start()
     {
         go = FindObjectOfType<LaserSpawn>();
         rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * force;
-
-
-
+        StartCoroutine(HomingMissile());
     }
+
+
+
+    private IEnumerator HomingMissile()
+    {
+        transform.parent = null;
+        speed = 0;
+        while (target != null)
+        {
+            Vector3 relativePos = target.position - transform.position - target.forward;  //Aiming at a point just behind the target so it always hits the target if coming fron the front, but never from behind
+            Quaternion rotation = Quaternion.LookRotation(Vector3.Scale(new Vector3(0.5f, 0.5f, 0.5f), relativePos + target.forward * 3));  //
+            rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+            transform.rotation = rotation;
+
+            speed = Mathf.Lerp(speed, 2f, 0.05f * Time.deltaTime);
+
+            transform.Translate(relativePos * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
 
-        lifeTime -= Time.deltaTime;
-        if(lifeTime <= 0)
-        {
-            Destroy(gameObject);
-        }
-        if(speed != 0)
-        {
-           // rb.AddForce(transform.forward * force);
-            //transform.position +=  dir * speed * Time.deltaTime;
-        } else
-        {
-            Debug.Log("No Speed");
-        }
+        //lifeTime -= Time.deltaTime;
+        //if(lifeTime <= 0)
+        //{
+        //    Destroy(gameObject);
+        //}
+        //if(speed != 0)
+        //{
+        //   // rb.AddForce(transform.forward * force);
+        //    //transform.position +=  dir * speed * Time.deltaTime;
+        //} else
+        //{
+        //    Debug.Log("No Speed");
+        //}
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Water" || collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Surface" || collision.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Water" /*|| other.gameObject.tag == "Enemy" */|| other.gameObject.tag == "Surface" || other.gameObject.tag == "Player")
         {
             speed = 0;
-            if (collision.gameObject.tag == "Enemy")
+            if (other.gameObject.tag == "Enemy")
             {
-                collision.gameObject.GetComponent<Enemy>().TakeDamage(100);
-            } else if(collision.gameObject.tag == "Player")
+                other.gameObject.GetComponent<Enemy>().TakeDamage(100);
+            } else if(other.gameObject.tag == "Player")
             {
-                collision.gameObject.GetComponent<Enemy>().TakeDamage(10);
-            } else 
-            {
-                Destroy(gameObject);
-            }
+                other.gameObject.GetComponent<Enemy>().TakeDamage(10);
+            } 
             Destroy(gameObject);
         }
     }
