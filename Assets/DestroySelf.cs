a9b8b101d87c99d36a73692b8583a22693455e07
Sid Naik <sid.naik27@gmail.com>
2019-10-13 13:22:22 -0400

    More Homing missile stuff

---
+++ Assets/DestroySelf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour
+{
+    public float time = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(selfdestruct());
+    }
+    private IEnumerator selfdestruct()
+    {
+        while (true)
+        {
+            time -= Time.deltaTime;
+            if (time < 0)
+                Destroy(transform.gameObject);
+            yield return null;
+        }
+    }
+}
