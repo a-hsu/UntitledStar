2eba602c4776b826f7f9db17c42a67499af6fa8b
Andrew Hsu <a-hsu@users.noreply.github.com>
2019-10-12 21:30:07 -0400

    Edited Intro scene

---
+++ Assets/EngineLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineLight : MonoBehaviour
+{
+    Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Random.Range(16f, 18f);
+    }
+}
